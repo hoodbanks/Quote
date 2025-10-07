f4216e2f1cb39da5e354cdb043741980f08f5c7b
hoodbanks <michaelchiemilie.com>
2025-10-07 08:39:28 +0100

    first commit

---
+++ js/index3.js
@@ -0,0 +1,27 @@
+// function declaration
+function sayHi(){
+    return "Hi"
+}
+
+// function expression
+
+const sayHello = function() {
+    return "hello"
+}
+
+//arrow function
+const sayHey = () => "hey";
+
+let x = 20
+
+// Global Scope
+
+function test() {
+    let y =20
+}
+ 
+
+const fruit = [" apple", "banana", "mango", "pawpaw","orange"
+    
+]
+
